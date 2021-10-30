49b2075fb50492dda7af685acb520f78128ab22a
Evgen Nechaev <i7utep.fm@gmail.com>
2021-10-30 23:27:48 +0300

    Edit hw4 -> Class Anagram

--- src/com/pb/nechaev/hw4/Anagram.java
+++ src/com/pb/nechaev/hw4/Anagram.java
@@ -1,9 +1,37 @@
 package com.pb.nechaev.hw4;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class Anagram {
 
 
+    public static boolean isAnagram(String strA, String strB) {
+
+        strA = strA.replaceAll("\\s+","").toLowerCase();
+        strB = strB.replaceAll("\\s+","").toLowerCase();
+
+
+        for (int i = 0; i < strA.length(); i++ ) {
+            if (strB.equals("")) return false;
+            strB = strB.replaceFirst(Pattern.quote("" + strA.charAt(i)), "");
+        }
+
+        return strB.equals("");
+    }
+
+
+
     public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Введите первую строку - ");
+        String st1 = in.nextLine();
+        System.out.print("Введите вторую строку - ");
+        String st2 = in.nextLine();
+
+        System.out.println(isAnagram(st1, st2));
 
     }
 }
